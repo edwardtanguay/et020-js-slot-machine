506e85114b11a13c8c851672552201b72eb4183d
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-01 21:02:03 +0200

    added windows and level button

--- main.js
+++ main.js
@@ -2,6 +2,22 @@ import "./style.css";
 
 document.querySelector("#app").innerHTML = /*html*/ `
 <div>
-  Hello, World!
+
+<div class="windows">
+	<div class="window1">111</div>
+	<div class="window2">222</div>
+	<div class="window3">333</div>
+</div>
+
+<button class="btnLever">Pull Level</button>
 </div>
 `;
+
+const btn = document.querySelector('.btnLever');
+btn.addEventListener('click', (e) => {
+	// const elemEmail = document.querySelector<HTMLInputElement>('.contactForm #email');
+	// const elemAge = document.querySelector<HTMLInputElement>('.contactForm #age');
+
+
+	alert('ok');
+});
