59dd9b426b1d8ff4a3c62232192e69f1daf98927
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-01 21:11:14 +0200

    shows all cherries

--- main.js
+++ main.js
@@ -13,15 +13,20 @@ document.querySelector("#app").innerHTML = /*html*/ `
 </div>
 `;
 
+const fruits = ['cherry', 'lemon', 'plum', 'apple', 'banana', 'grapes'];
+
+const getRandomeFruit = () => {
+	return fruits[0];
+}
+
 const btn = document.querySelector('.btnLever');
 btn.addEventListener('click', (e) => {
 	const window1Elem = document.querySelector('.window1');
 	const window2Elem = document.querySelector('.window2');
 	const window3Elem = document.querySelector('.window3');
 
-
-	window1Elem.innerHTML = 'nnn';
-	window2Elem.innerHTML = 'nnn';
-	window3Elem.innerHTML = 'nnn';
+	window1Elem.innerHTML = getRandomeFruit();
+	window2Elem.innerHTML = getRandomeFruit();
+	window3Elem.innerHTML = getRandomeFruit();
 
 });
